6f528e2e83c7ce86f0d8f550894f25c286239ffe
Matheus F <matheus_falmeida@hotmail.com>
2023-09-01 19:02:44 -0300

    Criação do Contexto para o componente ImagePreview

---
+++ src/components/Form/FileInput/Control.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { ChangeEvent, ComponentProps } from 'react'
+import { useFileInput } from './Root'
+
+export type ControlProps = ComponentProps<'input'>
+
+export function Control(props: ControlProps) {
+  const { id, onFilesSelected } = useFileInput()
+
+  function handleFilesSelected(event: ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files?.length) {
+      return
+    }
+
+    const files = Array.from(event.target.files)
+    // aqui eu to usando a função array para colocar os vários arquivos dentro de uma array
+
+    onFilesSelected(files)
+  }
+
+  return (
+    <input
+      type="file"
+      id={id}
+      className="sr-only"
+      {...props}
+      onChange={handleFilesSelected}
+    />
+  )
+}
