1d8a611f5dc50214738b122cfd06c79c1105111d
Matheus F <matheus_falmeida@hotmail.com>
2023-09-11 15:37:46 -0300

    Construção de variantes para o componente de File

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import * as FileInput from '@/components/Form/FileInput'
 import { Select } from '@/components/Form/Select/index'
 import { SelectItem } from '@/components/Form/Select/SelectItem'
 import { Textarea } from '@/components/Form/Textarea'
+import { Button } from '@/components/Button'
 
 export default function Home() {
   return (
@@ -24,19 +25,12 @@ export default function Home() {
           </div>
 
           <div className="flex flex-row items-center gap-3">
-            <button
-              className="h-10 w-20 rounded-lg border border-zinc-300 px-4 py-2.5 text-sm/5 font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-              type="button"
-            >
+            <Button variant="outline" type="button">
               Cancel
-            </button>
-            <button
-              className="h-1d0 w-16 rounded-lg bg-violet-600 px-4 py-2.5 text-sm/5 font-semibold text-white hover:bg-violet-700"
-              type="submit"
-              form="settings"
-            >
+            </Button>
+            <Button type="submit" form="settings">
               Save
-            </button>
+            </Button>
           </div>
         </div>
       </div>
@@ -150,43 +144,28 @@ export default function Home() {
               </Select>
 
               <div className="flex items-center">
-                <button
-                  type="button"
-                  className="rounded-md p-2 hover:bg-zinc-50"
-                >
+                <Button variant="ghost" type="button">
                   <Bold className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                </button>
+                </Button>
 
-                <button
-                  type="button"
-                  className="rounded-md p-2 hover:bg-zinc-50"
-                >
+                <Button variant="ghost" type="button">
                   <Italic className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                </button>
+                </Button>
 
-                <button
-                  type="button"
-                  className="rounded-md p-2 hover:bg-zinc-50"
-                >
+                <Button variant="ghost" type="button">
                   <Link className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                </button>
+                </Button>
 
-                <button
-                  type="button"
-                  className="rounded-md p-2 hover:bg-zinc-50"
-                >
+                <Button variant="ghost" type="button">
                   <List className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                </button>
+                </Button>
 
-                <button
-                  type="button"
-                  className="rounded-md p-2 hover:bg-zinc-50"
-                >
+                <Button variant="ghost" type="button">
                   <ListOrdered
                     className="h-4 w-4 text-zinc-500"
                     strokeWidth={3}
                   />
-                </button>
+                </Button>
               </div>
             </div>
 
@@ -215,18 +194,12 @@ export default function Home() {
         </div>
 
         <div className="flex flex-row items-center justify-end gap-3 pt-5">
-          <button
-            className="h-10 w-20 rounded-lg border border-zinc-300 px-4 py-2.5 text-sm/5 font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            type="button"
-          >
+          <Button variant="outline" type="button">
             Cancel
-          </button>
-          <button
-            className="h-1d0 w-16 rounded-lg bg-violet-600 px-4 py-2.5 text-sm/5 font-semibold text-white hover:bg-violet-700"
-            type="submit"
-          >
+          </Button>
+          <Button type="submit" form="settings">
             Save
-          </button>
+          </Button>
         </div>
       </form>
     </>
